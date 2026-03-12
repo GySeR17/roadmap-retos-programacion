a5d925cd43eadf088a5607b90e96c949df10ff1f
GySeR17 <jsmmm18@gmail.com>
2026-03-12 08:44:45 -0400

    120326_Gyser17_02

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/GySeR17.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/GySeR17.py
@@ -1,5 +1,19 @@
 """
-Funciones integradas (Built-in Functions)
+Lista de funciones posibles
+1. Funciones built-in
+2. Funciones definidas por el usuario
+3. Funciones dentro de funciones (HOF - Funciones de Orden Superior)
+4. Funciones anonimas(funciones lambda)
+5. Funciones de modulos
+6. Funciones de objetos (Metodos)
+7. Funciones recursivas
+8. Funciones generadoras
+9. Funciones decoradoras
+"""
+
+
+"""
+1. Funciones integradas (Built-in Functions)
 """
 
 #Algunas de las mas usadas pueden ser:
@@ -17,7 +31,7 @@ max(-6, 2, 5, 9, 4, 8, 3, 6, 36)    #Retorna el minimo
 range(2, 15)                        #Genera secuencias de numeros
 
 """
-Funciones definidas por el usuario
+2. Funciones definidas por el usuario
 """
 # simple
 
@@ -50,5 +64,96 @@ total_2 = mult(2, 5, 9)
 print(total_2)
 
 """
-Funciones dentro de funciones (HOF - Funciones de Orden Superior)
+3.Funciones dentro de funciones (HOF - Funciones de Orden Superior)
 """
+
+#Cualquier funcion que este definida por el usuario
+
+def aplicar(funcion, valor):
+    return funcion(valor)
+
+def cubo(x):
+    return x ** 3
+print(aplicar(cubo, 5))
+
+"""
+4. Funciones anonimas(funciones lambda)
+"""
+
+#Son funciones pequeñas sin nombre, usadas para operaciones simples
+suma = lambda a, b: a + b
+print(suma(3, 4))
+
+# Se usan mucho con map o sorted:
+numeros = [1, 2, 3, 4]
+doble = list(map(lambda x: x * 2, numeros)) #crear una lista, que con la funcion de map esta funcion lambda itera por cada numero de la lista:
+print(doble)
+
+"""
+5. Funciones de modulos
+"""
+#Se deben importar para poder ser usados:
+
+import math
+print(math.sqrt(16))
+
+import random
+print(random.randint(1, 15))
+
+"""
+6. Funciones de objetos (Metodos)
+"""
+texto_1 = "Hola Sergio!"
+print(texto_1.upper())
+
+numeros = [1, 2, 3, 4]
+numeros.append(5)
+print(numeros)
+
+
+"""
+7. Funciones recursivas
+"""
+
+#Son funciones que se llaman a si mismas
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+print(factorial(5))
+
+"""
+8. Funciones generadoras
+"""
+
+#Usan yield para generar valores uno por uno, sin almacenar toda la lista en memoria.
+def contar():
+    for i in range(5):
+        yield i 
+for numero in contar():
+    print(numero)
+
+"""
+9. Funciones decoradoras
+"""
+#Son funciones que modifican el comportamiento de otras funciones
+def decorador(func):
+    def nueva_funcion():
+        print("Antes de ejecutar")
+        func()
+        print("despues de ejecutar")
+    return nueva_funcion
+
+@decorador
+def saludo():
+    print("Hola!")
+
+saludo()
+
+"""
+Dificultad extra:
+"""
+
+def numeros():
